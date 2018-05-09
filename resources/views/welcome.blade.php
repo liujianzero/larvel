a040f3122160d86ad9d2b023be23a1e9866f2bb1
刘健 <396544421@qq.com>
2018-05-09 01:16:10 +0000

    procfile for heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - by zero 
+                    Laravel - by zero
                 </div>
 
                 <div class="links">
